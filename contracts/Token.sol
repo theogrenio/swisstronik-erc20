d2b132d8298295416ca562bc693236dd48d0b664
theogrenio <theogrenio@gmail.com>
2024-08-27 14:54:31 +0700

    firstPush

---
+++ contracts/Token.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20 {
+    constructor()ERC20("MemekHunter","MMKH"){} 
+
+    function mint100tokens() public {
+        _mint(msg.sender,100*10**18);
+    }
+
+    function burn100tokens() public{
+        _burn(msg.sender,100*10**18);
+    }
+    
+}
